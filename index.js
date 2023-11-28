4920a8a428f5f2e0014a3cc4a16039ad8f744b9f
Tanim1999 <ihtanim56@gmail.com>
2023-11-29 01:57:44 +0600

    completed asset request related api

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-    await client.connect();
+    // await client.connect();
      
     const productCollection = client.db('productDB').collection('product')
 
@@ -123,7 +123,7 @@ app.get('/cart/:userId/:productId', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
